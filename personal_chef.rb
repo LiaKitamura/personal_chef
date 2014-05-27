f70fc8919549d892a1085faf15b11dc1f91e8da9
Lia Kitamura <kit4mura@gmail.com>
2014-05-27 15:18:19 -0600

    made personal_chef file with instructions

---
+++ personal_chef.rb
@@ -0,0 +1,8 @@
+#create a personal chef class
+#make his name attribute accessible
+#make a method called make_toast that accepts a parameter
+  # (brown, light brown, burnt black etc...)
+#make a method called make_juice that accepts a parameter
+  #(orange, apple, cranberry)
+#make a method called make_eggs that accepts two parameters; the quantity of eggs
+    # (1,2,3, etc) and the style (overeasy, scrambled, sunnyside, etc...)
